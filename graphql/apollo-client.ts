7e85098e85e097e8cd6c1c2a7c90a944c49e6727
TaiNgo2112003 <Taingo2112003@gmail.com>
2025-08-08 23:50:50 +0700

    edit env and endpoint

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -2,7 +2,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/', 
+  uri: 'https://mock-graphql-server-tqzn.onrender.com',
+
   cache: new InMemoryCache(),
 });
 
